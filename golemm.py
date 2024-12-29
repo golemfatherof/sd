24702a050a23a4cde99b35987dad80cfa98804cc
golemfatherof <sminepl.ayzz@gmail.com>
2024-12-29 13:32:41 +0530

    Update golemm.py

--- golemm.py
+++ golemm.py
@@ -4,7 +4,7 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7839659347:AAGx0y2TNXOoI-V1XtD702MMb9w81We-KpI'
+API_TOKEN = '7003970033:AAHMBeDmxVqo87IkruhxGd3BiX5fEYluS0c'
 ADMIN_ID = '7584228621'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
